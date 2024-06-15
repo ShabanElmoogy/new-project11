a9ce4ac08d8be3a85d911acf005b353cfec5925d
ShabanElmogy <ShabanElmogy@DESKTOP-IGALHEL>
2024-06-15 13:49:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+  <>
+    <h1 className="title">Hello World</h1>
+    <h3>Hello World 2</h3>
+    <h3>Hello World 3</h3>
+    <h3>Hello World 3</h3>
+  </>
+  );
+}
+
+export default App;
